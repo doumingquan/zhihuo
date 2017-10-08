3957da1c032b520cec8224a749985e85c3d7e40f
MicroWin10-1203\Administrator <469688010@qq.com>
2017-10-08 16:32:37 +0800

    2017108

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -15,20 +15,27 @@ class Index extends Adminbase
            //完成数量
            $date=db('project')->where('is_delete!=2')->where('status=2')->count();
            $date2=db('project')->where('is_delete!=2')->where('status=1')->count();
+           $date3=db('project')->where('is_delete!=2')->where('status=3')->count();
+           
+           $BeginDate=date('Y-m-01', strtotime(date("Y-m-d")));
+           $Begin = strtotime($BeginDate);//echo $Begin;
+		   $last = strtotime("$BeginDate +1 month -1 day");
+		   //echo $lastDate;
+            $total1=db('project')->where('is_delete!=2')->where('status=1')->where('createtime','between',[$Begin,$last])->count();
+            $total2=db('project')->where('is_delete!=2')->where('status=2')->where('createtime','between',[$Begin,$last])->count();
+            $total3=db('project')->where('is_delete!=2')->where('status=3')->where('createtime','between',[$Begin,$last])->count();
+          	// echo $total1;echo $total2;echo $total3;
            $this->assign('date',$date);
            $this->assign('date2',$date2);
+           $this->assign('date3',$date3);
+           $this->assign('total1',$total1);
+           $this->assign('total2',$total2);
+           $this->assign('total3',$total3);
+        
+          // $this->assign('date3',$date3);
 
-           $data=Db::name('project')->alias('p')->field('p.*,po.position')->join('admin_position po' , 'po.id=p.pro_style_id')->where('is_delete!=2')->paginate(10);
-           foreach($data as $k=>$v){
-               $arr = explode(',',$v['admin_id']);
-               //dump($arr);
-               $array = array();
-               foreach ($arr as $vv){
-                   $admin_name = Db('admin')->field('id,username')->where(array('id'=>$vv))->find();
-                   array_push($array, $admin_name['username']);
-                   $data[$k]['username'] = implode(' ,',$array);
-               }
-           }
+           $data=Db::name('project')->alias('p')->field('p.*,po.position')->join('admin_position po' , 'po.id=p.pro_style_id')->where('is_delete!=2')->order('id desc')->paginate(10);
+
            // echo '<pre>';
            // print_r($data);
            // echo '</pre>';exit;
@@ -43,23 +50,22 @@ class Index extends Adminbase
            //完成数量
            $date=db('project')->where("FIND_IN_SET($id, admin_id)")->where('is_delete!=2')->where('status=2')->count();
            $date2=db('project')->where("FIND_IN_SET($id, admin_id)")->where('is_delete!=2')->where('status=1')->count();
+           $date3=db('project')->where("FIND_IN_SET($id, admin_id)")->where('is_delete!=2')->where('status=3')->count();
            $this->assign('date',$date);
            $this->assign('date2',$date2);
-
-           $data=Db::name('project')->alias('p')->field('p.*,po.position')->join('admin_position po' , 'po.id=p.pro_style_id')->where("FIND_IN_SET($id, admin_id)")->where('is_delete!=2')->paginate(10);
-           foreach($data as $k=>$v){
-               $arr = explode(',',$v['admin_id']);
-               //dump($arr);
-               $array = array();
-               foreach ($arr as $vv){
-                   $admin_name = Db('admin')->field('id,username')->where(array('id'=>$vv))->find();
-                   array_push($array, $admin_name['username']);
-                   $data[$k]['username'] = implode(' ,',$array);
-               }
-           }
-           // echo '<pre>';
-           // print_r($data);
-           // echo '</pre>';exit;
+           $this->assign('date3',$date3);
+           $BeginDate=date('Y-m-01', strtotime(date("Y-m-d")));
+           $Begin = strtotime($BeginDate);//echo $Begin;
+		   $last = strtotime("$BeginDate +1 month -1 day");
+		   //echo $lastDate;
+            $total1=db('project')->where("FIND_IN_SET($id, admin_id)")->where('is_delete!=2')->where('status=1')->where('createtime','between',[$Begin,$last])->count();
+            $total2=db('project')->where("FIND_IN_SET($id, admin_id)")->where('is_delete!=2')->where('status=2')->where('createtime','between',[$Begin,$last])->count();
+            $total3=db('project')->where("FIND_IN_SET($id, admin_id)")->where('is_delete!=2')->where('status=3')->where('createtime','between',[$Begin,$last])->count();
+            //echo $total1;echo $total2;echo $total3;
+            $this->assign('total1',$total1);
+           	$this->assign('total2',$total2);
+           	$this->assign('total3',$total3);
+           $data=Db::name('project')->alias('p')->field('p.*,po.position')->join('admin_position po' , 'po.id=p.pro_style_id')->where("FIND_IN_SET($id, admin_id)")->where('is_delete!=2')->order('id desc')->paginate(10);
            $assign=array(
                'data'=>$data
            );
